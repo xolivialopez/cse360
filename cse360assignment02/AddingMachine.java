0b8a9850e93214281e8cd48092d309c80d73787f
xolivialopez <72430044+xolivialopez@users.noreply.github.com>
2020-10-05 23:39:36 -0700

    version 1

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+// @author Olivia Lopez
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+	
+	// return total
+	public int getTotal() {
+		return 0;
+	}
+	
+	// add parameter value to total
+	public void add(int value) {
+	}
+
+	// subtract parameter value from total
+	public void subtract(int value) {
+	}
+	
+	// history of transactions starting from 0
+	public String toString() {
+		return "";
+	}
+	
+	// clear memory
+	public void clear() {
+	}
+}
